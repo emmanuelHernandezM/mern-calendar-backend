052204081ef5857e01bc29f0072d495b1c52e9eb
Emmanuel Hernández Maqueda <emmanuel.hernandezm@softtek.com>
2022-02-06 16:59:22 -0600

    BACKEND 1.0 terminado

---
+++ routes/events.js
@@ -0,0 +1,34 @@
+// Rutas de Eventos / Events
+// host + /api/events
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { isDate } = require('../helpers/isDate');
+
+const router = Router();
+
+// Todas tienen que pasar por la validacion del JWT
+router.use( validarJWT );
+
+// Obtener eventos
+router.get('/', getEventos);
+// Crear un nuevo evento
+router.post(
+    '/', 
+    [   //middlewares
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'Fecha de finalizacion es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento  
+);
+// Actualizar evento
+router.put('/:id', actualizarEvento );
+// Borrar evento
+router.delete('/:id', eliminarEvento );
+
+module.exports = router;
